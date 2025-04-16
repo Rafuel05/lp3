cbaed9865d164f92326748751ed42ff0c5e9d506
Rafael Melchior <118372879+Rafuel05@users.noreply.github.com>
2025-04-16 16:57:49 -0400

    aula 16/04/2025

--- produtos/models.py
+++ produtos/models.py
@@ -4,7 +4,7 @@ from categoria.models import Categoria
 
 # Create your models here.
 class Produto(models.Model):
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null =True)
     produto_nome = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=210, unique=True)
     descricao = models.TextField(max_length=300)
@@ -13,6 +13,6 @@ class Produto(models.Model):
     estoque = models.IntegerField()
     esta_disponivel = models.BooleanField(default=True)
     modificado_em= models.DateTimeField(auto_now=True)
-    criado_em= models.DateTimeField(auto_now_add=True)
+    criado_em= models.DateTimeField(auto_now_add=True, null=True)
 
     
